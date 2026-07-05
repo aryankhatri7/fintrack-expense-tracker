7a335fb9c789a6abe2e1267895c03cda6545e4b5
Aryan Khatri <mr.aryankhatri@gmail.com>
2026-07-05 12:27:29 +0530

    Setup backend with Express and MongoDB Atlas

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import dns from "node:dns";
+import connectDB from "./config/db.js";
+
+// Load environment variables
+dotenv.config();
+
+// Force Google DNS
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("FinTrack Backend Running 🚀");
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
